d6064875fd889423de7dd3d71c73bc40f6bd1500
flowww13 <florent.sauvetage@gmail.com>
2020-01-20 13:34:01 +0100

    first commit

---
+++ lib/app.rb
@@ -0,0 +1,5 @@
+require 'dotenv'# Appelle la gem Dotenv
+
+Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
+# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+
